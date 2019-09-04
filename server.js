7c2b8544e849e63dc244de7b93ea35b48ebf30aa
Dominic Torruellas <dom@domtorr.com>
2019-09-04 14:17:31 -0700

    model set, test for model, jest setup

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 
+const { getFooBars } = require('./data/models/fooBarModel');
+
 const server = express();
 
 server.use(express.json());
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'Server working' });
+});
+
+server.get('/fooBars', async (req, res) => {
+  try {
+    return res.status(200).json(await getFooBars());
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
 module.exports = server;
